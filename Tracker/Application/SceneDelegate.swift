9c11742ad7020b536f14a24ba6b8a556b9194a8b
H3ARTgg <nomaxismax@gmail.com>
2023-04-03 18:29:04 +0300

    Refactor

--- Tracker/Application/SceneDelegate.swift
+++ Tracker/Application/SceneDelegate.swift
@@ -5,7 +5,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = TrackerListViewController()
         window?.makeKeyAndVisible()
     }
 }
--- Tracker/ViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-final class ViewController: UIViewController {
-    let defaultView = UIView(frame: .zero)
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-}
-
